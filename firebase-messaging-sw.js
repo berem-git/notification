c80bb1fe42c26d62a3c2350a3a6d8e4bf81d5e22
Berembek Babaev <berem-git>
2024-02-28 21:22:47 +0600

    commit message

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,35 @@
-// [START initialize_firebase_in_sw]
-importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-  'messagingSenderId': '<%= SENDER_ID %>',
-});
+    apiKey: 'AIzaSyDMf8LGtCOipiic6cSO8m6THwTBUKJUwoA',
+    authDomain: 'notification-50717.firebaseapp.com',
+    projectId: 'notification-50717',
+    storageBucket: 'notification-50717.appspot.com',
+    messagingSenderId: '967435761286',
+    appId: '1:967435761286:web:ba5a4d77f65c4f72d08307',
+  });
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
-// [END initialize_firebase_in_sw]
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body
+    };
+  
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
