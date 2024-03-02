3798b087b72e5d97e7c0e8b739e427a6c49c2724
berembek.babaev <berembek.babaev@optimabank.kg>
2024-03-02 15:38:39 +0600

    1

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener("push", (event) => {
+    const notification = event.data.json().notification;
+    const data = event.data.json();
+    event.waitUntil(self.registration.showNotification(notification.title , {
+        body: notification.body,
+        icon: '/icon.png',
+        vibrate: true,
+        data: {
+            url: data.click_action
+        },
+        actions: [
+            { action: "aceptar", title: "Aceptar" },
+            { action: "rechazar", title: "Rechazar" },
+        ],
+    }));
+});
+
+self.addEventListener("notificationclick", (event) => {
+    console.log('event2', event)
+    if(!event?.data?.title) event.waitUntil(clients.openWindow(event.notification.data.url));
+    if (event.data.title === "aceptar") {
+        return event.waitUntil(clients.openWindow(event.notification.data.url));
+    }
+    if (event.data.title === "rechazar") {
+        return  console.log("Botón rechazar pulsado");
+    }
+
+});
