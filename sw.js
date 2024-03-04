76dbc3145a9cdd41b5b05df68c5f575b5b94af5c
berembek.babaev <berembek.babaev@optimabank.kg>
2024-03-04 09:38:43 +0600

    1

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener("push", (event) => {
     const notification = event.data.json().notification;
     const data = event.data.json();
-    console.log(data)
     event.waitUntil(self.registration.showNotification(notification.title , {
         body: notification.body,
         icon: '/icon.png',
@@ -9,22 +8,11 @@ self.addEventListener("push", (event) => {
         data: {
             url: data.data.click_action
         },
-        actions: [
-            { action: "home", title: "Домой" },
-            { action: "about", title: "О нас" },
-        ],
     }));
 });
 
 self.addEventListener("notificationclick", (event) => {
-    console.log('event2', event)
     const link = event.notification.data.url
-    if(!event?.data?.title) event.waitUntil(clients.openWindow(link));
-    if (event.data.title === "home") {
-        return event.waitUntil(clients.openWindow(link));
-    }
-    if (event.data.title === "about") {
-        return event.waitUntil(clients.openWindow('/notification'));
-    }
-
+    event.waitUntil(clients.openWindow(link));
+    event.notification.close();
 });
